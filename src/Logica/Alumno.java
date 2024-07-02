2b87334d2dae250add3bab1101d31471718c482f
Jefferson Alquinga <144504631+Jeffers199817@users.noreply.github.com>
2024-07-02 13:02:48 -0500

    Finalización de los metodos getters and setters

--- src/Logica/Alumno.java
+++ src/Logica/Alumno.java
@@ -17,13 +17,53 @@ public class Alumno {
     
     
     //Métodos son metodos con verbor en infinivos
+    //Constructor Vacio
+    public Alumno(){ 
+        
+        
+    }
     
+    //Contructor lleno
     public Alumno(int id, String nombre, String apellido){ 
         this.id = id;
         this.nombre=nombre;
         this.apellido = apellido;
         
     }
+    
+    public int getId(){
+        return id;
+    }
+    
+    
+    public void setId( int id){
+        
+        this.id=id;
+        
+    }
+    
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+        
+    }
+    
+    
+    public String getApellido(){
+        return apellido;
+    }
+    
+    
+    public void setApellido(String apellido){
+         this.apellido = apellido;
+    }
+    
+    
     
     public void mostraNombre(){
         System.out.println("su nombre es:" + nombre);
--- src/Logica/POO.java
+++ src/Logica/POO.java
@@ -16,14 +16,33 @@ public class POO {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        
+        Alumno estudiante1 = new Alumno();
      Alumno estudiante= new Alumno( 1,"Jefferson", "Alquinga");
+     
+     
      
      estudiante.mostraNombre();
      estudiante.saberAprobado(7);
      
-     
-    
+        System.out.println("La id del alumno 2: " + estudiante.getId());
+        int idNuevo= 2;
+        estudiante.setId(idNuevo);
+        System.out.println("El nuevo id es: "+ estudiante.getId());
+        
+        System.out.println("======================================");
+        System.out.println("Datos del alumno 1 son:");
+        System.out.println(" id: "+ estudiante1.id+ " nombre: " + estudiante1.nombre + " apellido: " + estudiante1.apellido );
+        
+        System.out.println("Seteamos los datos del alumno1 ");
+        
+        estudiante1.setId(1);
+        estudiante1.setNombre("Jefferson1");
+        estudiante1.setApellido("chuquimarca1");
+        
+         System.out.println("======================================");
+        System.out.println("Datos del alumno 1 actualizados son:");
+        System.out.println(" id: "+ estudiante1.id+ " nombre: " + estudiante1.nombre + " apellido: " + estudiante1.apellido );
+        
     }
     
 }
